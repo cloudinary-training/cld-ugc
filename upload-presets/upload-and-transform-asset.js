5a8a5646d8c88d24177d58aa6187a444636fd983
Jen Brissman <brissman514@gmail.com>
2023-10-16 21:16:30 +0200

    adding assets for examples

---
+++ upload-presets/upload-and-transform-asset.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.uploader.upload('./assets/smiling.jpg', {
+  public_id: 'smiling',
+  upload_preset: 'profile-preset'
+})
+.then(uploadResult => console.log(uploadResult))
+.catch(error => console.error(error));
