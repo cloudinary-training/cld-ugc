4e1b7b768f9956c6791cfcacfe3289fdef45655d
Jen Brissman <brissman514@gmail.com>
2023-10-20 15:57:40 +0200

    clarifying foldering options

--- foldering/specify-location.js
+++ foldering/specify-location.js
@@ -6,13 +6,15 @@ const cloudinary = require('cloudinary').v2;
 // this means the image of the sushi platter is in a folder called dishes which is nested within another folder called restaurant 38
 // which is nested within another folder called california 
 
-cloudinary.uploader.upload("./assets/sushi.jpg", {public_id: "california/restaurant38/dishes/omakase-platter-3"})
+cloudinary.uploader.upload("./assets/omakase-platter-3.jpg",
+{public_id: "california/restaurant38/dishes/omakase-platter-3"})
 	.then(result => {console.log(result)})
 	.catch(error => {console.log(error)});
 
 // specifying the folder only, which will be included in the public ID
 // once uploaded, the public ID for this asset will be: store77/fall-coats-2023/[unique string of 20 characters] (i.e. store77/fall-coats-2023/l7xrssxeqrvfh7kch5dc)
 
-cloudinary.uploader.upload("./assets/model-with-bangs.jpg", {folder: "store77/fall-coats-2023"})
+cloudinary.uploader.upload("./assets/model-with-bangs.jpg",
+{folder: "store77/fall-coats-2023"})
 	.then(result => {console.log(result)})
 	.catch(error => {console.log(error)});
