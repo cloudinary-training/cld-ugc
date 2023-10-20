a2156e0c72ba696763418ad70bb68d4acb4b9af2
Jen Brissman <brissman514@gmail.com>
2023-10-20 17:23:12 +0200

    testing threshhold for blurriness

--- moderation/image-moderation/upload-with-eval.js
+++ moderation/image-moderation/upload-with-eval.js
@@ -7,8 +7,18 @@ const cloudinary = require('cloudinary').v2;
 // This can be useful for conditionally adding tags, contextual metadata and structured metadata depending on specific criteria of the uploaded file.
 
 // using eval parameter to assign a tag of "blurry" to any images that have been evaluated under the designated focus score of .5
+// alternatively, you could also query on quality_analysis.pixel_score
+
+//The quality_score, quality_analysis.color_score and quality_analysis.pixel_score fields can be used in the search method when
+// quality_analysis is set to true in the upload request or an upload preset. 
+
 cloudinary.uploader
 .upload("./assets/headshot-blurry.jpg",
   { quality_analysis: true,
     eval: "if (resource_info.quality_analysis.focus < 1) { upload_options['tags'] = 'blurry'}" })
 .then(result=>console.log(result));
+
+// Another option is to evaluate the accessibility analysis score upon upload.
+// DOCS: https://cloudinary.com/documentation/analysis_on_upload#accessibility_analysis
+// Analyzing your images for accessibility can help you to choose the best images for people with color blindness.
+
