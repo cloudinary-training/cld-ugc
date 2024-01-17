b9020b448a94c8c8803e66b6ca90e29742beaa70
Jen Brissman <brissman514@gmail.com>
2024-01-16 20:41:07 -0800

    correcting upload preset

--- upload-presets/create-upload-preset.js
+++ upload-presets/create-upload-preset.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
 // creating a simple upload preset with tagging, foldering, accepting only certain file formats
-// you'll notices that we have have not included additional transformations in this upload preset
+// you'll notice that we have have not included additional transformations in this upload preset
 // check out "create-derived-image-preset.js" (in this same upload-presets folder of the repo) to see how to create an upload preset WITH transformations included
 cloudinary.api.create_upload_preset({
   name: 'cozy-babies',
